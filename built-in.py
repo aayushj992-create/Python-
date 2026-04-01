4c50fc875451c4b8b7c27e01c99ee1bfe580ecfe
Aayush <aayushj992@gmail.com>
2026-04-01 17:05:14 +0000

    studied built functions

---
+++ built-in.py
@@ -0,0 +1,23 @@
+list=[1,2,3,4,5,6]
+print(len(list))
+
+print(max(list))
+print(min(list))
+
+
+result=5
+prefix="string"
+print(str(result)+prefix)
+
+for i in range(5,15,2):
+    print(i)
+
+
+mystr="The quick, brown big dog bites very hard"
+for each in range(0,len(mystr),2):
+    print(mystr[each])
+
+
+greeting="hello"
+count=10
+print(f"{greeting} you are visitor number {count}")
