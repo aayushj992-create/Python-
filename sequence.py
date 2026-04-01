11a8750775abfe12905dba25744bcf9395a652c4
Aayush <aayushj992@gmail.com>
2026-04-01 15:56:29 +0000

    python learning

---
+++ sequence.py
@@ -0,0 +1,25 @@
+mylist=[0,1,"two",3.2,False]
+
+# print(mylist[1:4:2])
+# print(mylist)
+# print(mylist[0])
+# print(mylist[-1])
+# mylist[1]=100
+# print(mylist)
+
+# anotherlist=[6,7,8]
+# mylist=mylist+anotherlist
+# print(mylist)
+
+# mystr="This is a string"
+# print(mystr[2])
+
+mytuple=(0,1,2,"three")
+print(mytuple)
+
+mysets={0,2,21,21,31,41,1233,12,1}
+print(mysets)
+
+print(1 in mylist)
+print(3 in mytuple)
+print(2 in mysets)
---
+++ variable.py
@@ -0,0 +1,34 @@
+myint= 10
+myfloat= 13.2567
+mystr="hello"
+bool=True
+
+#variable need to start with chracter
+
+print(myint)
+print(mystr)
+
+print(myint+myfloat)
+print(myint-myfloat)
+print(myint*myfloat)
+print(myint%3)
+
+another_str="world"
+print(mystr+another_str)
+print('nom'*3)
+
+
+print(myint==10)
+print(myfloat!=10)
+print(myint>20)
+print(myfloat<10)
+
+print(myint>5 and myfloat<25.0)
+
+print(myint>5 or myfloat<5)
+
+print(not(myint>5 and myfloat<25.0))
+
+
+myint="abc"
+print(myint)
