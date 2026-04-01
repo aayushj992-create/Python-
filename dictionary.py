c806e7de2c2b04320e1e402da07161c0a356d397
Aayush <aayushj992@gmail.com>
2026-04-01 16:03:01 +0000

    dictionaries

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+mydict={"one":1,"two":2,3:"three",4.5:["four","point","five"]}
+print(mydict)
+
+print(mydict["one"])
+print(mydict[3])
+
+
+mydict["seven"]=7
+print(mydict)
+
+
+print("two" in mydict)
+print("blarg" in mydict)
+
+
+print(mydict.keys())
+print(mydict.values())
+
+for key,val in mydict.items():
+    print(key,val)
